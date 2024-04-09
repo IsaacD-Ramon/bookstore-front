c56d6f01a90c13780867150ba26a1622dc1cdf16
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-04-09 15:52:56 -0300

    chamando component

--- bookstore-front/src/app/app.component.ts
+++ bookstore-front/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
- templateUrl:'app.component.html'
+  imports: [HeaderComponent],
+ templateUrl:'app.component.html',
 })
 export class AppComponent {
   title = 'bookstore-front';
